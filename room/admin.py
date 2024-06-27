d67f572ad8f13e43a775665323ccc26dca6c4d35
Mojtaba Sabbagh <mojtaba.sabbagh@yahoo.com>
2024-06-27 11:26:08 +0330

    Admin more improvements.

--- room/admin.py
+++ room/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import Room, Message
+
 # Register your models here.
-admin.site.register(Room)
-admin.site.register(Message)
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
+    css = {
+             'all': ('templates/css/admin-extra.css ',)
+        }
+    list_filter = ('room',)
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    css = {
+             'all': ('templates/css/admin-extra.css ',)
+        }
+    readonly_fields = ('id',)
