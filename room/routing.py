b7319464bc7a560104120b54ab51b8bd86824f7e
Mojtaba Sabbagh <mojtaba.sabbagh@yahoo.com>
2024-07-04 21:33:02 +0330

    adding route to chat

--- room/routing.py
+++ room/routing.py
@@ -3,6 +3,6 @@ from django.urls import path, re_path
 from . import consumers, refresh
 
 websocket_urlpatterns = [
-    re_path(r"ws/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
-     re_path(r"ws/(?P<user_name>\w+)/$", refresh.RefreshRoom.as_asgi()),
+    re_path(r"chat/ws/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
+     re_path(r"chat/ws/(?P<user_name>\w+)/$", refresh.RefreshRoom.as_asgi()),
 ]
