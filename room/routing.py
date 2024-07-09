317e2b911d3ee5ac7d20fffd7c722e3283c31212
Mojtaba Sabbagh <mojtaba.sabbagh@yahoo.com>
2024-07-09 18:51:24 +0330

    new route added

--- room/routing.py
+++ room/routing.py
@@ -3,5 +3,6 @@ from django.urls import path, re_path
 from room import consumers
 
 websocket_urlpatterns = [
+    re_path(r"/chat/ws/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
     re_path(r"chat/ws/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
 ]
